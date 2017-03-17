3c2aa5b2960dee0cd8fc8a78275d7b8b195fd163
Tetiana Novakivska <tetpekker@gmail.com>
2017-03-17 20:25:24 +0200

    Homework. Not completed

---
+++ src/main/java/com/novakivska/app/homework/TrainingMethods.java
@@ -0,0 +1,22 @@
+package com.novakivska.app.homework;
+
+/**
+ * Created by Tas_ka on 3/16/2017.
+ */
+public class TrainingMethods {
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int modullo(int a, int b) {
+        return a % b;
+    }
+
+    public static String reverse(String a) {
+        String b = new StringBuilder(a).reverse().toString();
+        return b;
+
+    }
+}
+
---
+++ src/test/java/com/novakivska/app/homework/TrainingMethodsTest.java
@@ -0,0 +1,36 @@
+package com.novakivska.app.homework;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Tas_ka on 3/16/2017.
+ */
+public class TrainingMethodsTest {
+
+    int a = 30;
+    int b = 30;
+
+    TrainingMethods method = new TrainingMethods();
+
+    @Test
+    public void devision() {
+        int actualResult = method.divide(a, b);
+        int expectedResult = 1;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void modullo(){
+        int actualResult = method.modullo(a, b);
+        int expectedResult = 0;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void reverse(){
+        String actualResult = method.reverse("test");
+        String expectedResult = "tset";
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
